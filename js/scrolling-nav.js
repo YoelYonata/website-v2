47c9c7c6d23df13668cb85c8e9be151c81e9a59a
Yoel Yonata <yoel.yonata@gmail.com>
2020-02-06 23:39:47 -0800

    Fixed smooth scrolling

--- js/scrolling-nav.js
+++ js/scrolling-nav.js
@@ -15,22 +15,11 @@
     }
   });
 
-  // Closes responsive menu when a scroll trigger link is clicked
-  $('.js-scroll-trigger').click(function() {
-    $('.navbar-collapse').collapse('hide');
-  });
-
   // Activate scrollspy to add active class to navbar items on scroll
   $('body').scrollspy({
     target: '#mainNav',
     offset: 56
   });
 
-  $(function(){
-    $('#NeuroKinetic_modal').modal({
-        show: false
-    }).on('hidden.bs.modal', function(){
-        $(this).find('neurokineticvideo')[0].pause();
-    });
 
 })(jQuery); // End of use strict
